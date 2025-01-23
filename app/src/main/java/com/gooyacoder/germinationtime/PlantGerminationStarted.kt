24a6eff6ba95c406860c5a73c8fa2ace4a0b6b5a
gooyacoder <gooyacoder@gmail.com>
2025-01-23 14:35:32 +0330

    4_11_1403  14:35

--- app/src/main/java/com/gooyacoder/germinationtime/PlantGerminationStarted.kt
+++ app/src/main/java/com/gooyacoder/germinationtime/PlantGerminationStarted.kt
@@ -1,5 +1,9 @@
 package com.gooyacoder.germinationtime
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
@@ -9,8 +13,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.FileNotFoundException
 
 class PlantGerminationStarted : AppCompatActivity() {
+
+    var imageBitmap: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,11 +33,44 @@ class PlantGerminationStarted : AppCompatActivity() {
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
                 if (event?.action == MotionEvent.ACTION_DOWN) {
                     // Perform your action here
-                    Toast.makeText(applicationContext, "Add Seed Image", Toast.LENGTH_LONG).show()
+                    //Toast.makeText(applicationContext, "Add Seed Image", Toast.LENGTH_LONG).show()
+                    val intent = Intent(Intent.ACTION_PICK)
+                    intent.type = "image/*"
+                    val mimeTypes = arrayOf("image/jpeg", "image/png")
+                    intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+                    startActivityForResult(intent, 2)
                     return true
                 }
                 return false
             }
         })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val imageView = findViewById<ImageView>(R.id.seedImage)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            imageBitmap = data?.extras?.get("data") as Bitmap
+            //val plant_preview: ImageView = findViewById(R.id.plant_preview)
+            imageView.setImageBitmap(imageBitmap)
+        }
+        else if (requestCode == 2 && resultCode == RESULT_OK){
+            var selectedImage: Uri? = null
+            if (data != null) {
+                selectedImage = data?.data
+                try {
+                    val input = contentResolver.openInputStream(selectedImage!!)
+                    val selectedImg = BitmapFactory.decodeStream(input)
+                    imageBitmap = selectedImg
+                    //val plant_preview: ImageView = findViewById(R.id.plant_preview)
+                    imageView.setImageBitmap(imageBitmap)
+
+                } catch (e: FileNotFoundException) {
+                    e.printStackTrace()
+                    Toast.makeText(this, "An error occurred!", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
     }
 }
